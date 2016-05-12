f136cfeea31bf169b5c619b6580274517195c680
Thinkpad <Thinkpad@Lenovo>
2016-05-12 17:28:24 -0400

    squelete de code

---
+++ src/model/Contact.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author shahin.behrooz@gmail.com
+ */
+public class Contact {
+    //les attribut d<un contact
+    private String name;
+    private String tel;
+    // validator pour chaque attribut
+    private Validator nameValidator = new NameValidator();
+    private Validator telValidator = new TelValidator();
+    
+    public void setName(String name) throws InvalidFieldException{
+        nameValidator.validate(name);
+        this.name = name;
+    }
+    public void setNameValidator(Validator v){
+        nameValidator = v;
+    }
+
+    String getId() {
+//todo ajouter id
+    }
+    /** 
+     * todo: a ajouter les autres setters selon exemple
+     * */
+    
+}
---
+++ src/model/ContactsCollection.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Hashtable;
+
+/**
+ *
+ * @author Thinkpad
+ */
+public class ContactsCollection {
+    private Hashtable<String, Contact> hashTable;
+    public ContactsCollection(){
+        hashTable = new Hashtable<String, Contact>();
+    }
+    public void addContact(Contact c){
+        hashTable.put(c.getId(), c);
+    }
+    public void load(Hashtable<String, Contact> source){
+        hashTable.putAll(source);
+    }
+    public void deleteContact(String id){
+        hashTable.remove(id);
+    }
+}
